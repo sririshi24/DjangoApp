877dff4b471f716ee051d2ef5123a32963471163
Rishikesan S <sririshi24@gmail.com>
2020-11-30 17:17:22 +0530

    test 2 from pc

--- Edxweb/manage.py
+++ Edxweb/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks here."""
+    """Run administrative tasks can be done here."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edxweb.settings')
     try:
         from django.core.management import execute_from_command_line
