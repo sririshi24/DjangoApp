108148c720c13f637e4b011fbd15c54c48278e46
sririshi24 <sririshi24@gmail.com>
2020-11-30 17:25:51 +0530

    from satserver

--- Edxweb/manage.py
+++ Edxweb/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks here."""
+    """Run administrative tasks here from satserver"""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Edxweb.settings')
     try:
         from django.core.management import execute_from_command_line
